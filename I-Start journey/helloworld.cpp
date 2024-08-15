ccf3a2512b2ae4c5dc721c22bb5b434d260342a0
revlis3m <gmansour042000@gmail.com>
2024-08-15 21:58:03 +0000

    First commit

---
+++ I-Start journey/helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World !" << std::endl;
+    std::cout << "Salam le monde !" << std::endl; //Affiche le message hello world
+    return 0;
+}
